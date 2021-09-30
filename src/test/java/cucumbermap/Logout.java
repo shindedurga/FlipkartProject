b64d62e127cdc8a69e4dec9b70c4433460f8f386
shindedurga <shinde.durga590@gmail.com>
2021-09-29 16:38:23 -1200

    By Ms.Durga

---
+++ src/test/java/cucumbermap/Logout.java
@@ -0,0 +1,20 @@
+package cucumbermap;
+
+public class Logout
+{
+    public void log()
+    {
+    	
+    	System.out.println("1234");
+    	
+     }
+	public static void main(String[]args)
+	{
+		Logout obj=new Logout();
+		obj.log();
+		
+		
+	}
+	
+	
+}
